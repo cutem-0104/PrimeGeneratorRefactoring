a9de9f9a92bf9b41cfe623f9d51a15c54dc73612
cutem-0104 <shuhei.sugaya@gmail.com>
2019-06-24 23:32:55 +0900

    クラス名変更、メイン関数を3つの関数に分離

--- PrimeGenerator.java
+++ PrimeGenerator.java
@@ -1,13 +1,7 @@
 /**
  * このクラスは素数を生成する。生成される素数の上限はユーザーが指定する。
  * ここで使用しているアルゴリズムは、「エラトステネスのふるい」法である。
- * <p>
- * キュレネのエラトステネスは紀元前２７６年のリビアのキュレネに生まれ、
- * 紀元前１９４年アレキサンドリアで没した。最初に地球の全周を計算した人である。
- * また、閏年の暦の作成に取り組んだことでも知られ、アレキサンドリアの
- * 図書館を管理していた人物でもある。
- * <p>
- * このアルゴリズムｈ極めて単純である。２から始まる整数配列を与え、２の
+ * このアルゴリズムは極めて単純である。２から始まる整数配列を与え、２の
  * 倍数をすべて消す。まだ消えていない次の整数を見つけ、その倍数を
  * すべて削除する。一番大きい数の平方根を超えるまで、この作業を繰り返す。
  * 
@@ -15,53 +9,70 @@
  * @version 9 Dec 1999 rcm
  */
 public class GeneratePrimes {
+    private static int s;
+    private static boolean[] f;
+    private static int[] primes;
+
     /**
      * @param maxValueは、生成する素数の上限
      */
     public static int[] generatePrimes(int maxValue) {
-        if (maxValue >= 2) { // 有効なケース？
-            // 宣言
-            int s = maxValue + 1; // 配列のサイズ
-            boolean[] f = new Boolean[s];
-            int i;
-
-            // 配列を真(true)に初期化
-            for (i = 0; i < s; i++) {
-                f[i] = true;
-            }
-
-            // 周知の非素数を取り除く
-            f[0] = f[1] = false;
-
-            // ふるい落とす
-            int j;
-            for (i = 2; i < Math.sqrt(s) + 1; i++) {
-                if (f[i]) { // iが除かれていなければ、その倍数を除く
-                    for (j = 2 * i; j < s; j += i) {
-                        f[j] = false; // 倍数は素数ではない
-                    }
-                }
-            }
-
-            // 見つけた素数の個数をカウント
-            int count = 0;
-            for (i = 0; i < s; i++) {
-                if (f[i]) {
-                    count++; // カウントアップ
-                }
-            }
-
-            int[] primes = new int[count];
-
-            // 素数の抜出
-            for (i = 0, j = 0; i < s; i++) {
-                if (f[i]) { // 素数であれば
-                    primes[j++] = i;
-                }
-            }
-            return primes; // 素数を返す
+        if (maxValue < 2) {
+            return new int[0];
         } else {
-            return new int[0]; // 入力が不適切な場合null配列を戻す
+            initializeSieve(maxValue);
+            sieve();
+            loadPrimes();
+            return primes;// 素数をリターン
         }
+    }
+
+    private static void loadPrimes() {
+        int i;
+        int j;
+
+        // 見つけた素数の個数をカウント
+        int count = 0;
+        for (i = 0; i < s; i++) {
+            if (f[i]) {
+                count++; // カウントアップ
+            }
+        }
+
+        int[] primes = new int[count];
+
+        // 素数の抜出
+        for (i = 0, j = 0; i < s; i++) {
+            if (f[i]) { // 素数であれば
+                primes[j++] = i;
+            }
+        }
+    }
+
+    private static void sieve() {
+        int i;
+        int j;
+        for (i = 2; i < Math.sqrt(s) + 1; i++) {
+            if (f[i]) { // iが除かれていなければ、その倍数を除く
+                for (j = 2 * i; j < s; j += i) {
+                    f[j] = false; // 倍数は素数ではない
+                }
+            }
+        }
+    }
+
+    private static void initializeSieve(int maxValue) {
+        // 宣言
+        s = maxValue + 1;
+        f = new Boolean[s];
+        int i;
+
+        // 配列を真（true）に初期化
+        for (i = 0; i < s; i++) {
+            f[i] = true;
+        }
+
+        // 周知の非素数を取り除く
+        f[0] = f[1] = false;
     }
 }
